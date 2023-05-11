47eb74e5ccfb351137fefadba108b500b4f565d8
Jgarkoo <105646764+Jgarkoo@users.noreply.github.com>
2023-05-11 16:51:37 +0400

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,9 +1,12 @@
 // Use following code to generate a random number between 0 and 9. : `Math.floor(Math.random() * 10);`Write a while loop and print 'not 5' in every iteration until number 5 gets randomly generated.
 
 
-let randomNumber;
+let randomNumber = -1;
 
-do {
-  randomNumber = Math.floor(Math.random() * 10);
-  console.log("not 5");
-} while (randomNumber !== 5);
+while(randomNumber !== 5) {
+    randomNumber = Math.floor(Math.random() * 10);
+  
+    if(randomNumber !== 5) {
+        console.log('not 5');
+    }
+}
